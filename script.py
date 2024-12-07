c212cd944c9ba6b37bb4813008eb95212b79010c
HorekiSun <mankevychdima@gmail.com>
2024-12-07 18:22:05 +0200

    add setup script

--- script.py
+++
@@ -1,6 +0,0 @@
-from pytubefix import YouTube
-
-url = input("Enter video url: ")
-video = YouTube(url)
-downloader = video.streams.get_highest_resolution()
-downloader.download()
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/dash
+
+pip install -r requirements.txt
+python3 app.py
